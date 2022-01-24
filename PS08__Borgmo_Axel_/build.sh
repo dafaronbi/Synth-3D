a0235efb06aa65a0366ea2eed4e4017c477de261
dafaronbi <>
2022-01-23 23:46:18 -0500

    got rid of uneeded files

--- PS08__Borgmo_Axel_/build.sh
+++
@@ -1,3 +0,0 @@
-gcc -Wall -o synth main.c synth.c key_tables.c paUtils.c \
-	-I/usr/local/include \
-	-L/usr/local/lib -lsndfile -lportaudio -lncurses
--- PS09 __Borgmo_Axel_/build.sh
+++
@@ -1,4 +0,0 @@
-g++ -Wno-deprecated -o synth \
-	main.cpp synth.cpp key_tables.c paUtils.c \
-	-I/usr/local/include \
-	-L/usr/local/lib -lsndfile -lportaudio -lncurses
